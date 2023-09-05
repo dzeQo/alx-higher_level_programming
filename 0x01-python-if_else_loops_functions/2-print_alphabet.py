da44429b80e569812357ce41c4fbb3ad2a782191
dzeQo <zaki.lbssir@gmail.com>
2023-09-05 16:09:14 +0000

    t8

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+ = 97
+while a <= 122:
+    print("{}".format(chr(a)), end="")
+    a += 1
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+ = 97
+while a <= 122:
+    if a == 101 or a == 113:
+        a += 1
+        continue
+    print("{}".format(chr(a)), end="")
+    a += 1
